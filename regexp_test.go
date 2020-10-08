f97e50db25ecf8a4237fe75a2bc1bd287cebb122
Dmitry Panov <dop@itoolabs.com>
2020-10-08 10:41:07 +0100

    Fixed handling of empty submatches in unicode strings. Fixes #210

--- regexp_test.go
+++ regexp_test.go
@@ -481,6 +481,14 @@ func TestRegexpOverrideSpecies(t *testing.T) {
 	testScript1(SCRIPT, _undefined, t)
 }
 
+func TestRegexp2InvalidEscape(t *testing.T) {
+	testScript1(`/(?=)\x0/.test("x0")`, valueTrue, t)
+}
+
+func TestRegexpUnicodeEmptyMatch(t *testing.T) {
+	testScript1(`/(0)0|/gu.exec("0\xef").length === 2`, valueTrue, t)
+}
+
 func BenchmarkRegexpSplitWithBackRef(b *testing.B) {
 	const SCRIPT = `
 	"aaaaaaaaaaaaaaaaaaaaaaaaa++bbbbbbbbbbbbbbbbbbbbbb+-ccccccccccccccccccccccc".split(/([+-])\1/)
