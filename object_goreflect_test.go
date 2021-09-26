6e5fa0950d16122cfb1a7bf8a8bcd52c499e389e
Dmitry Panov <dop@itoolabs.com>
2021-09-26 13:00:33 +0100

    Prefer the exported value when doing type conversions to preserve wrapped values. Only use reflect.Convert() if the types' kinds match. Closes #329.

--- object_goreflect_test.go
+++ object_goreflect_test.go
@@ -3,9 +3,11 @@ package goja
 import (
 	"errors"
 	"fmt"
+	"math"
 	"reflect"
 	"strings"
 	"testing"
+	"time"
 )
 
 func TestGoReflectGet(t *testing.T) {
@@ -1167,5 +1169,25 @@ func TestGoReflectUnicodeProps(t *testing.T) {
 	`)
 	if err != nil {
 		t.Fatal(err)
+	}
+}
+
+func TestGoReflectPreserveType(t *testing.T) {
+	vm := New()
+	var expect = time.Duration(math.MaxInt64)
+	vm.Set(`make`, func() time.Duration {
+		return expect
+	})
+	vm.Set(`handle`, func(d time.Duration) {
+		if d.String() != expect.String() {
+			t.Fatal(`expect`, expect, `, but get`, d)
+		}
+	})
+	_, e := vm.RunString(`
+	var d=make()
+	handle(d)
+	`)
+	if e != nil {
+		t.Fatal(e)
 	}
 }
