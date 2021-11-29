4e41734c41ede65398d06ca049cb40dea70cb45f
Mihail Stoykov <M.Stoikov@gmail.com>
2021-11-29 10:37:19 +0200

    Simplify ascii string concat and skip some allocations
    With this simplified code it skips some allocations of byte slices
    
    ```
     benchstat old.bench new.bench
     name           old time/op    new time/op    delta
     ASCIIConcat-8    44.3µs ± 5%    40.2µs ±12%   -9.18%  (p=0.000 n=10+10)
    
     name           old alloc/op   new alloc/op   delta
     ASCIIConcat-8    14.0kB ± 0%     9.9kB ± 0%  -29.28%  (p=0.000 n=10+10)
    
     name           old allocs/op  new allocs/op  delta
     ASCIIConcat-8       131 ± 0%       121 ± 0%   -7.63%  (p=0.000 n=10+10)
    ```

--- string_test.go
+++ string_test.go
@@ -12,3 +12,19 @@ func TestStringOOBProperties(t *testing.T) {
 
 	testScript1(SCRIPT, valueInt(1), t)
 }
+
+func BenchmarkASCIIConcat(b *testing.B) {
+	vm := New()
+
+	b.ResetTimer()
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		_, err := vm.RunString(`{let result = "ab";
+		for (let i = 0 ; i < 10;i++) {
+			result += result;
+		}}`)
+		if err != nil {
+			b.Fatalf("Unexpected errors %s", err)
+		}
+	}
+}
