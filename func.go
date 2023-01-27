8a0df7296ebdd6b66ab2bb8230f9744dc8824825
Dmitry Panov <dop@itoolabs.com>
2023-01-27 13:28:31 +0000

    Reworked AsyncContextTracker

--- func.go
+++ func.go
@@ -19,16 +19,22 @@ var (
 	resultAwaitMarker = NewSymbol("await")
 )
 
-// AsyncContextTracker is a handler that allows to track async function's execution context. Every time an async
-// function is entered, the Entered method is called. The trackingObject it returns is remembered and
-// the next time just before the context is resumed, Resumed is called with the same trackingObject as argument.
-// Suspended is called when the async function suspends on await. Completed is called when the async function returns or throws.
+// AsyncContextTracker is a handler that allows to track an async execution context to ensure it remains
+// consistent across all callback invocations.
+// Whenever a Promise reaction job is scheduled the Grab method is called. It is supposed to return the
+// current context. The same context will be supplied to the Resumed method before the reaction job is
+// executed. The Exited method is called after the reaction job is finished.
+// This means that for each invocation of the Grab method there will be exactly one subsequent invocation
+// of Resumed and then Exited methods (assuming the Promise is fulfilled or rejected). Also, the Resumed/Exited
+// calls cannot be nested, so Exited can simply clear the current context instead of popping from a stack.
+// Note, this works for both async functions and regular Promise.then()/Promise.catch() callbacks.
+// See TestAsyncContextTracker for more insight.
+//
 // To register it call Runtime.SetAsyncContextTracker().
 type AsyncContextTracker interface {
-	Entered() (trackingObject interface{})
-	Suspended()
+	Grab() (trackingObject interface{})
 	Resumed(trackingObject interface{})
-	Completed()
+	Exited()
 }
 
 type funcObjectImpl interface {
@@ -620,15 +626,9 @@ type asyncRunner struct {
 	promiseCap *promiseCapability
 	f          *Object
 	vmCall     func(*vm, int)
-
-	trackingObj interface{}
 }
 
 func (ar *asyncRunner) onFulfilled(call FunctionCall) Value {
-	if tracker := ar.f.runtime.asyncContextTracker; tracker != nil {
-		tracker.Resumed(ar.trackingObj)
-		ar.trackingObj = nil
-	}
 	ar.gen.vm.curAsyncRunner = ar
 	defer func() {
 		ar.gen.vm.curAsyncRunner = nil
@@ -640,10 +640,6 @@ func (ar *asyncRunner) onFulfilled(call FunctionCall) Value {
 }
 
 func (ar *asyncRunner) onRejected(call FunctionCall) Value {
-	if tracker := ar.f.runtime.asyncContextTracker; tracker != nil {
-		tracker.Resumed(ar.trackingObj)
-		ar.trackingObj = nil
-	}
 	ar.gen.vm.curAsyncRunner = ar
 	defer func() {
 		ar.gen.vm.curAsyncRunner = nil
@@ -657,9 +653,6 @@ func (ar *asyncRunner) onRejected(call FunctionCall) Value {
 func (ar *asyncRunner) step(res Value, done bool, ex *Exception) {
 	r := ar.f.runtime
 	if done || ex != nil {
-		if tracker := r.asyncContextTracker; tracker != nil {
-			tracker.Completed()
-		}
 		if ex == nil {
 			ar.promiseCap.resolve(res)
 		} else {
@@ -669,9 +662,6 @@ func (ar *asyncRunner) step(res Value, done bool, ex *Exception) {
 	}
 
 	// await
-	if tracker := r.asyncContextTracker; tracker != nil {
-		tracker.Suspended()
-	}
 	promise := r.promiseResolve(r.global.Promise, res)
 	promise.self.(*Promise).addReactions(&promiseReaction{
 		typ:         promiseReactionFulfill,
@@ -690,9 +680,6 @@ func (ar *asyncRunner) start(nArgs int) {
 	ar.promiseCap = r.newPromiseCapability(r.global.Promise)
 	sp := r.vm.sp
 	ar.gen.enter()
-	if tracker := r.asyncContextTracker; tracker != nil {
-		ar.trackingObj = tracker.Entered()
-	}
 	ar.vmCall(r.vm, nArgs)
 	res, resType, ex := ar.gen.step()
 	ar.step(res, resType == resultNormal, ex)
--- func_test.go
+++ func_test.go
@@ -161,47 +161,88 @@ func ExampleAssertConstructor() {
 	// Output: Test
 }
 
+type testAsyncCtx struct {
+	group    string
+	refCount int
+}
+
 type testAsyncContextTracker struct {
-	groupNamePtr *string
+	ctx     *testAsyncCtx
+	logFunc func(...interface{})
+	resumed bool
 }
 
-func (s testAsyncContextTracker) Entered() interface{} {
-	return *s.groupNamePtr
+func (s *testAsyncContextTracker) Grab() interface{} {
+	ctx := s.ctx
+	if ctx != nil {
+		s.logFunc("Grab", ctx.group)
+		ctx.refCount++
+	}
+	return ctx
 }
 
-func (s testAsyncContextTracker) Suspended() {
-	*s.groupNamePtr = ""
+func (s *testAsyncContextTracker) Resumed(trackingObj interface{}) {
+	s.logFunc("Resumed", trackingObj)
+	if s.resumed {
+		panic("Nested Resumed() calls")
+	}
+	s.ctx = trackingObj.(*testAsyncCtx)
+	s.resumed = true
 }
 
-func (s testAsyncContextTracker) Resumed(trackingObj interface{}) {
-	*s.groupNamePtr = trackingObj.(string)
+func (s *testAsyncContextTracker) releaseCtx() {
+	s.ctx.refCount--
+	if s.ctx.refCount < 0 {
+		panic("refCount < 0")
+	}
+	if s.ctx.refCount == 0 {
+		s.logFunc(s.ctx.group, "is finished")
+	}
 }
 
-func (s testAsyncContextTracker) Completed() {
-	*s.groupNamePtr = ""
+func (s *testAsyncContextTracker) Exited() {
+	s.logFunc("Exited")
+	if s.ctx != nil {
+		s.releaseCtx()
+		s.ctx = nil
+	}
+	s.resumed = false
 }
 
 func TestAsyncContextTracker(t *testing.T) {
 	r := New()
-	var groupName string
+	var tracker testAsyncContextTracker
+	tracker.logFunc = t.Log
 
 	group := func(name string, asyncFunc func(FunctionCall) Value) Value {
-		prevGroupName := groupName
+		prevCtx := tracker.ctx
 		defer func() {
-			groupName = prevGroupName
+			t.Log("Returned", name)
+			tracker.releaseCtx()
+			tracker.ctx = prevCtx
 		}()
-		groupName = name
+		tracker.ctx = &testAsyncCtx{
+			group:    name,
+			refCount: 1,
+		}
+		t.Log("Set", name)
 		return asyncFunc(FunctionCall{})
 	}
-	r.SetAsyncContextTracker(testAsyncContextTracker{groupNamePtr: &groupName})
+	r.SetAsyncContextTracker(&tracker)
 	r.Set("group", group)
 	r.Set("check", func(expectedGroup, msg string) {
+		var groupName string
+		if tracker.ctx != nil {
+			groupName = tracker.ctx.group
+		}
 		if groupName != expectedGroup {
 			t.Fatalf("Unexpected group (%q), expected %q in %s", groupName, expectedGroup, msg)
 		}
+		t.Log("In", msg)
 	})
 
-	_, err := r.RunString(`
+	t.Run("", func(t *testing.T) {
+		_, err := r.RunString(`
 		group("1", async () => {
 		  check("1", "line A");
 		  await 3;
@@ -214,8 +255,55 @@ func TestAsyncContextTracker(t *testing.T) {
 		}).then(() => {
             check("", "line E");
 		})
+		`)
+		if err != nil {
+			t.Fatal(err)
+		}
+	})
+
+	t.Run("", func(t *testing.T) {
+		_, err := r.RunString(`
+		group("some", async () => {
+			check("some", "line A");
+		    (async () => {
+				check("some", "line B");
+		        await 1;
+				check("some", "line C");
+		        await 2;
+				check("some", "line D");
+		    })();
+			check("some", "line E");
+		});
 	`)
-	if err != nil {
-		t.Fatal(err)
-	}
+		if err != nil {
+			t.Fatal(err)
+		}
+	})
+
+	t.Run("", func(t *testing.T) {
+		_, err := r.RunString(`
+	group("Main", async () => {
+		check("Main", "0.1");
+		await Promise.all([
+			group("A", async () => {
+				check("A", "1.1");
+				await 1;
+				check("A", "1.2");
+			}),
+			(async () => {
+				check("Main", "3.1");
+			})(),
+			group("B", async () => {
+				check("B", "2.1");
+				await 2;
+				check("B", "2.2");
+			})
+		]);
+		check("Main", "0.2");
+	});
+	`)
+		if err != nil {
+			t.Fatal(err)
+		}
+	})
 }
