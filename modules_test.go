a0920e6cfa271fba7bfa8c898360442e03646498
Mihail Stoykov <M.Stoikov@gmail.com>
2023-10-26 12:00:35 +0300

    Refactor modules_test.go to be more easy to work with

--- modules_test.go
+++ modules_test.go
@@ -2,10 +2,8 @@ package goja
 
 import (
 	"fmt"
-	"io/fs"
 	"sync"
 	"testing"
-	"testing/fstest"
 )
 
 func TestSimpleModule(t *testing.T) {
@@ -14,111 +12,109 @@ func TestSimpleModule(t *testing.T) {
 		m   ModuleRecord
 		err error
 	}
-	type testCase struct {
-		fs fs.FS
-		a  string
-		b  string
-	}
 
-	testCases := map[string]testCase{
+	testCases := map[string]map[string]string{
 		"function export": {
-			a: `import { b } from "dep.js";
-globalThis.s = b()
-`,
-			b: `export function b() {globalThis.p(); return 5 };`,
+			"a.js": `
+				import { b } from "dep.js";
+				globalThis.s = b()
+			`,
+			"dep.js": `export function b() { return 5 };`,
 		},
 		"let export": {
-			a: `import { b } from "dep.js";
-globalThis.s = b()
-`,
-			b: `export let b = function() {globalThis.p(); return 5 };`,
+			"a.js": `
+				import { b } from "dep.js";
+				globalThis.s = b()
+			`,
+			"dep.js": `export let b = function() {return 5 };`,
 		},
 		"const export": {
-			a: `import { b } from "dep.js";
-globalThis.s = b()
-`,
-			b: `export const b = function() {globalThis.p(); return 5 };`,
+			"a.js": `
+				import { b } from "dep.js";
+				globalThis.s = b()
+			`,
+			"dep.js": `export const b = function() { return 5 };`,
 		},
 		"let export with update": {
-			a: `import { s , b} from "dep.js";
-      s()
-globalThis.s = b()
-`,
-			b: `export let b = "something";
-        export function s(){
-        globalThis.p()
-          b = function() {globalThis.p(); return 5 };
-        }`,
+			"a.js": `
+				import { s , b} from "dep.js";
+				s()
+				globalThis.s = b()
+			`,
+			"dep.js": `
+				export let b = "something";
+				export function s(){
+					b = function() {
+						return 5;
+					};
+				}`,
 		},
 		"default export": {
-			a: `import b from "dep.js";
-globalThis.s = b()
-`,
-			b: `export default function() {globalThis.p(); return 5 };`,
+			"a.js": `
+				import b from "dep.js";
+				globalThis.s = b()
+			`,
+			"dep.js": `export default function() { return 5 };`,
 		},
 		"default loop": {
-			a: `import b from "a.js";
-export default function() {return 5;};
-globalThis.s = b()
-`,
-			b: ``,
+			"a.js": `
+				import b from "a.js";
+				export default function() {return 5;};
+				globalThis.s = b()
+			`,
 		},
 		"default export arrow": {
-			a: `import b from "dep.js";
-			globalThis.p();
-globalThis.s = b();
-`,
-			b: `globalThis.p(); export default () => {globalThis.p(); return 5 };`,
+			"a.js": `
+				import b from "dep.js";
+				globalThis.s = b();
+			`,
+			"dep.js": `export default () => {return 5 };`,
 		},
 		"default export with as": {
-			a: `import b from "dep.js";
-globalThis.s = b()
-`,
-			b: `function f() {return 5;};
-      export { f as default };`,
+			"a.js": `
+				import b from "dep.js";
+				globalThis.s = b()
+			`,
+			"dep.js": `
+				function f() {return 5;};
+				export { f as default };
+			`,
 		},
 		"export usage before evaluation as": {
-			a: `import  "dep.js";
-            export function a() {return 5;}
-`,
-			b: `import { a } from "a.js";
-           globalThis.s = a();`,
+			"a.js": `
+				import  "dep.js";
+				export function a() { return 5; }
+			`,
+			"dep.js": `
+				import { a } from "a.js";
+				globalThis.s = a();
+			`,
 		},
 		"dynamic import": {
-			a: `
-			globalThis.p();
-import("dep.js").then((imported) => {
-			globalThis.p()
-	globalThis.s = imported.default();
-});`,
-			b: `export default function() {globalThis.p(); return 5;}`,
+			"a.js": `
+				import("dep.js").then((imported) => {
+					globalThis.s = imported.default();
+				});
+			`,
+			"dep.js": `export default function() { return 5; }`,
 		},
 		"dynamic import error": {
-			a: ` do {
-  import('dep.js').catch(error => {
-			if (error.name == "SyntaxError") {
-globalThis.s = 5;
-			}
-  });
-} while (false);
-`,
-			b: `
-import { x } from "0-fixture.js";
+			"a.js": `
+				do {
+					import('dep.js').catch(error => {
+						if (error.name == "SyntaxError") {
+							globalThis.s = 5;
+						}
+					});
+				} while (false);
 			`,
-			fs: &fstest.MapFS{
-				"0-fixture.js": &fstest.MapFile{
-					Data: []byte(`
+			"dep.js": `import { x } from "0-fixture.js";`,
+			"0-fixture.js": `
 					export * from "1-fixture.js";
-					export * from "2-fixture.js"; 
-				`),
-				},
-				"1-fixture.js": &fstest.MapFile{
-					Data: []byte(`export var x`),
-				},
-				"2-fixture.js": &fstest.MapFile{
-					Data: []byte(`export var x`),
-				},
-			},
+					export * from "2-fixture.js";
+			`,
+			"1-fixture.js": `export var x`,
+			"2-fixture.js": `export var x`,
 		},
 	}
 	for name, cases := range testCases {
@@ -135,19 +131,8 @@ import { x } from "0-fixture.js";
 				if ok {
 					return k.m, k.err
 				}
-				var src string
-				switch specifier {
-				case "a.js":
-					src = cases.a
-				case "dep.js":
-					src = cases.b
-				default:
-					b, err := fs.ReadFile(cases.fs, specifier)
-					if err != nil {
-						panic(specifier)
-					}
-					src = string(b)
-				}
+
+				src := string(cases[specifier])
 				p, err := ParseModule(specifier, src, hostResolveImportedModule)
 				if err != nil {
 					cache[specifier] = cacheElement{err: err}
@@ -186,19 +171,7 @@ import { x } from "0-fixture.js";
 				m := m
 				t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
 					t.Parallel()
-					var err error
 					vm := New()
-					vm.Set("p", vm.ToValue(func() {
-						// fmt.Println("p called")
-					}))
-					vm.Set("l", func(v Value) {
-						fmt.Printf("%+v\n", v)
-						fmt.Println("l called")
-						fmt.Printf("iter stack ; %+v\n", vm.vm.iterStack)
-					})
-					if err != nil {
-						t.Fatalf("got error %s", err)
-					}
 					eventLoopQueue := make(chan func(), 2) // the most basic and likely buggy event loop
 					vm.SetImportModuleDynamically(func(referencingScriptOrModule interface{}, specifierValue Value, pcap interface{}) {
 						specifier := specifierValue.String()
@@ -214,9 +187,8 @@ import { x } from "0-fixture.js";
 						}
 					})
 					var promise *Promise
-					eventLoopQueue <- func() {
-						promise = m.Evaluate(vm)
-					}
+					eventLoopQueue <- func() { promise = m.Evaluate(vm) }
+
 				outer:
 					for {
 						select {
@@ -226,9 +198,10 @@ import { x } from "0-fixture.js";
 							break outer
 						}
 					}
+
 					if promise.state != PromiseStateFulfilled {
 						t.Fatalf("got %+v", promise.Result().Export())
-						err = promise.Result().Export().(error)
+						err := promise.Result().Export().(error)
 						t.Fatalf("got error %s", err)
 					}
 					v := vm.Get("s")
