5ef83b82af1782927af1a3ec258cd88971746df4
Dmitry Panov <dop@itoolabs.com>
2025-01-25 21:32:03 +0000

    Correctly handle null or undefined second argument to Function.prototype.apply(). Fixes #655

--- builtin_function_test.go
+++ builtin_function_test.go
@@ -12,3 +12,10 @@ func TestHashbangInFunctionConstructor(t *testing.T) {
 	`
 	testScriptWithTestLib(SCRIPT, _undefined, t)
 }
+
+func TestFunctionApplyNullArgArray(t *testing.T) {
+	const SCRIPT = `
+	assert.sameValue(0, (function() {return arguments.length}).apply(undefined, null))
+	`
+	testScriptWithTestLib(SCRIPT, _undefined, t)
+}
