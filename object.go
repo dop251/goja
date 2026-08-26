8f1c0696a37b221d3b14dc4c2e826cc22a6b723d
Dmytro Manchynskyi <xtrafrancyz@gmail.com>
2026-08-26 23:49:18 +0300

    Fix a bunch of skipped tc39 tests (#729)
    * Add Error.isError and fix Error(Symbol()) tests
    
    * Fix setPrototypeOf(Object.prototype, {}) must fail
    
    * Fix Array.flat(undefined)
    
    * Fix tc39/AggregateError/errors-iterabletolist.js
    
    * Implement JSON.rawJson, Json.isRawJSON

--- object.go
+++ object.go
@@ -26,6 +26,7 @@ const (
 	classRegExp        = "RegExp"
 	classDate          = "Date"
 	classJSON          = "JSON"
+	classRawJSON       = "RawJSON"
 	classGlobal        = "global"
 	classPromise       = "Promise"
 
@@ -452,6 +453,12 @@ func (o *baseObject) setProto(proto *Object, throw bool) bool {
 	current := o.prototype
 	if current.SameAs(proto) {
 		return true
+	}
+	// 10.4.7 Immutable Prototype Exotic Objects
+	// Object.prototype seems like the only immutable prototype exotic object
+	if o.val == o.val.runtime.global.ObjectPrototype {
+		o.val.runtime.typeErrorResult(throw, "Immutable prototype object 'Object.prototype' cannot have their prototype set")
+		return false
 	}
 	if !o.extensible {
 		o.val.runtime.typeErrorResult(throw, "%s is not extensible", o.val)
